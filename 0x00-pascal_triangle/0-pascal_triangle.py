75b17214836042490095e8055e84b1fed4a6bf98
bonface murangiri <bonnyrangi95@gmail.com>
2024-08-16 05:01:40 -0700

    Implement initial Pascal's Triangle generator

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+    generates Pascal's Triangle up to the nth row.
+    Args:
+        n (int): Number of rows of Pascal's Triangle to generate.
+    Returns:
+        list: A list of lists, where each inner list represents a row of Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+    
+    triangle = []
+
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+
+    return triangle
