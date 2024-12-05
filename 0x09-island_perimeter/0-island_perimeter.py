cee071848b344ba1a22033b09a4f116be43876ec
Boniface Murangiri <bonnyrangi95@gmail.com>
2024-12-05 07:05:52 +0000

    Island Perimeter.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,9 +6,9 @@ def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in the grid.
     Parameters:
-        grid (list[list[int]]): 2D grid of integers where 0 represents 
+        grid (list[list[int]]): 2D grid of integers where 0 represents
         water and 1 represents land.
-                            
+
     Returns:
         int: Perimeter of the island.
     """
@@ -16,16 +16,12 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
-                #top
                 if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
-                #bottom
-                if row == len(grid) - 1 or grid[row + 1][col] = 0:
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
                     perimeter += 1
-                #left
                 if col == 0 or grid[row][col - 1] == 0:
                     perimeter += 1
-                #right
                 if col == len(grid[0]) - 1 or grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
